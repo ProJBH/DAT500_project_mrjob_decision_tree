4f2e66770707b4b21c04fb4c818a8b5c48732d89
ProJBH <nzbohuajia@gmail.com>
2022-04-29 19:00:55 +0200

    upload files

---
+++ mrjob decision tree from scratch/preduce_subtrain_csv_map.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+for line in sys.stdin:
+    line = re.sub("[\"\']", "", line)
+    #line = line.replace('\'','')
+    if line.find('Train') == 0:
+            line = line.strip()
+            value = line.split("\t")
+            if len(value)!=0:
+                value = ((value[1].lstrip("[")).rstrip("]")).replace(" ","")
+                print(value)
